3fe4d5f74d48a93047c4a407ae8b04b79859fe98
Hugo Montes <hugomontes927@gmail.com>
2021-01-11 21:38:40 -0400

    Aplicando PropTypes al componente Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,25 @@
 // Importar React
 import React from 'react'
-
+// Importar PropTyptes
+import PropTypes from 'prop-types'
 
 // Componente funcional
 // Componente que muestra un boton
 const Button = ({text, clickHandler}) => {
     return (
         <button onClick={ () => {
-            console.log("onClick en el Button")
             // Invocar a la funcion obtenida enviando text
             clickHandler(text)
         }}>
             <span>{text}</span>
         </button>
     )
+}
+
+// Validar las propiedades
+Button.propTypes = {
+    text: PropTypes.string.isRequired, 
+    clickHandler: PropTypes.func.isRequired
 }
 
 // Exportar el componente
