d5d356507644233cc99bf9eac09b21ad724846a0
hmontes <hmontes@cofar.com.bo>
2021-01-13 16:14:59 -0400

    Mejorando el codigo usando DRY

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -3,20 +3,30 @@ import React from 'react'
 import Proptypes from 'prop-types'
 import Button from './Button/Button'
 
+// Generar un array origen con los 
+// numeros de cada boton de forma ordenada
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+// Crear un funcion
+// que renderize parte de nuestro componente
+// La funcion recibe un parametro onClickNumber
+const renderButtons = onClickNumber => {
+    // Iterar numbers y transformar en un componente Button
+    const renderButton = number => (
+        <Button key={number} text={number.toString()} clickHandler={onClickNumber} />
+    )
+    // retornar usando la funcion map
+    return numbers.map(renderButton)
+}
+
 // Crear la constante para la funcion
 // Agregar el parametro una funcion onClickNumber destructurandolo
 const Numbers = ({onClickNumber}) => (
     <section className="numbers">
-        <Button text="1" clickHandler={onClickNumber} />
-        <Button text="2" clickHandler={onClickNumber} />
-        <Button text="3" clickHandler={onClickNumber} />
-        <Button text="4" clickHandler={onClickNumber} />
-        <Button text="5" clickHandler={onClickNumber} />
-        <Button text="6" clickHandler={onClickNumber} />
-        <Button text="7" clickHandler={onClickNumber} />
-        <Button text="8" clickHandler={onClickNumber} />
-        <Button text="9" clickHandler={onClickNumber} />
-        <Button text="0" clickHandler={onClickNumber} />
+        {
+            // Invocar a la funcion para generar los botones
+            renderButtons(onClickNumber)
+        }
     </section>
 )
 
